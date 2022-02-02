2cfca48d8d88d5441d28110f01e44edf79d620bc
breponte <bkreponte@ucsd.edu>
2022-02-02 15:59:43 -0800

    Fix jUnit lib?

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,9 @@
+import org.junit.*;
 import static org.junit.Assert.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-
-import org.junit.*;
 
 public class MarkdownParseTest {
     @Test

cf873a5cf68082880cbb94d0366ba6fd56126d80
breponte <bkreponte@ucsd.edu>
2022-02-02 16:10:45 -0800

    Updated paths for tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,63 +13,63 @@ public class MarkdownParseTest {
 
     @Test
     public void testOriginalFile() throws IOException {
-        Path fileName = Path.of("C:/Users/bkeko/Documents/GitHub/markdown-parse-clone2/break0.md");
+        Path fileName = Path.of("break0.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testBreakFile1() throws IOException {
-        Path fileName = Path.of("C:/Users/bkeko/Documents/GitHub/markdown-parse-clone2/break1.md");
+        Path fileName = Path.of("break1.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testBreakFile2() throws IOException {
-        Path fileName = Path.of("C:/Users/bkeko/Documents/GitHub/markdown-parse-clone2/break2.md");
+        Path fileName = Path.of("break2.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of("https://something().com"), MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testBreakFile3() throws IOException {
-        Path fileName = Path.of("C:/Users/bkeko/Documents/GitHub/markdown-parse-clone2/break3.md");
+        Path fileName = Path.of("break3.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of(), MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testBreakFile4() throws IOException {
-        Path fileName = Path.of("C:/Users/bkeko/Documents/GitHub/markdown-parse-clone2/break4.md");
+        Path fileName = Path.of("break4.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testBreakFile5() throws IOException {
-        Path fileName = Path.of("C:/Users/bkeko/Documents/GitHub/markdown-parse-clone2/break5.md");
+        Path fileName = Path.of("break5.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of(), MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testBreakFile6() throws IOException {
-        Path fileName = Path.of("C:/Users/bkeko/Documents/GitHub/markdown-parse-clone2/break6.md");
+        Path fileName = Path.of("break6.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of(), MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testBreakFile7() throws IOException {
-        Path fileName = Path.of("C:/Users/bkeko/Documents/GitHub/markdown-parse-clone2/break7.md");
+        Path fileName = Path.of("break7.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of(), MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testBreakFile8() throws IOException {
-        Path fileName = Path.of("C:/Users/bkeko/Documents/GitHub/markdown-parse-clone2/break8.md");
+        Path fileName = Path.of("break8.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of(), MarkdownParse.getLinks(contents));
     }
